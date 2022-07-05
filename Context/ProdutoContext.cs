511cccb2a8880a6f0bee716ec280a26d21af359f
Mathh PDS <mth.pedrosa@outlook.com>
2022-07-04 21:36:44 -0300

    Adicionar arquivos de projeto.

---
+++ Context/ProdutoContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiTestePratico.Context
+{
+    public class ProdutoContext : DbContext
+    {
+        public ProdutoContext(DbContextOptions<ProdutoContext> options) : base(options) { }
+
+        public object Produto { get; internal set; }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiTestePratico.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public int Estoque { get; set; }
+
+        public string  Descricao { get; set; }
+    }
+}
